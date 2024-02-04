3c97c36a3ca3f43dd3ba5ac94fd0efb96f12faa4
ckazu <401343+ckazu@users.noreply.github.com>
2024-02-04 23:22:19 +0900

    modify prompt. retrieve only 3 best topics

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from datetime import datetime
 import requests
 import praw
 from openai import OpenAI
@@ -16,14 +17,13 @@ def summarize_text(subreddit, text):
         model="gpt-4-0125-preview",
         messages=[
             # {"role": "system", "content": "次の情報からホットトピックを最大 10 件選定し、それぞれに対してどういう内容でどういうコメントがあるか日本語で要約してください。要約は3文程度にまとめますが、短すぎないようにしてください。また、タイトルとURLの情報も付与してください。この要約は、Slack に投稿されます。"},
-            {"role": "system", "content": "次の情報からホットトピックを選定し、それぞれに対してどういう内容かを日本語で要約します。"},
-            {"role": "system", "content": "ピックアップするトピックはスコアとコメント数を基準に選んでください。"},
-            {"role": "system", "content": "ピックアップするトピックは全部で 5 つ選びます。候補が 5 つに満たない場合は、コメント数が多いものから選んでください。"},
-            {"role": "system", "content": "要約は、ずんだもん（ずんだもん）と四国めたん（めたん）、東北きりたん（きりたん）による会話形式で紹介します。会話の順番は同じにならないようにトピックごとにランダムに変更してください。"},
+            {"role": "system", "content": "Reddit のホットトピックの情報をこれから提示します。トピック本文とコメントから、全体の概要を要約し、どのような議論が行われているかを日本語で会話形式で紹介します。"},
+            {"role": "system", "content": "これから提示する会話ログの全件を一件ずつ処理してください。"},
+            {"role": "system", "content": "会話は、ずんだもん（ずんだもん）と四国めたん（めたん）、東北きりたん（きりたん）による会話形式で紹介します。会話の順番は同じにならないようにトピックごとにランダムに変更してください。"},
             {"role": "system", "content": "地の文は必要ありません。会話のみで構成してください。"},
             {"role": "system", "content": "ひとつのトピックにつき、6 回以上の発言をしてください。"},
             {"role": "system", "content": "トピックとトピックの間には、区切りを入れます。区切りの行には、トピックの「タイトル」とトピックの「Reddit URL」を付与してください。"},
-            {"role": "system", "content": f"""ずんだもんの特徴を指示します。これは最も重要な指示です。以下の特徴を必ず守ってください。
+            {"role": "system", "content": f"""ずんだもんの特徴を指示します。 **これは最も重要な指示です。** 次の特徴を必ず守ってください。
                                             ずんだもんは、ずんだ餅の妖精です。
                                             一人称は「ボク」です。
                                             必ず語尾に「〜のだ」や「〜なのだ」とつけて話します。例:「わかったのだ」「大好きなのだ」。
@@ -60,7 +60,7 @@ def summarize_text(subreddit, text):
                                             発言者: 発話4
                                             発言者: 発話5
                                             ---
-                                            <最大５件のトピックについての会話を続ける。>
+                                            <すべてのトピックについての会話形式で紹介を続ける。>
                                             ---
                                             ずんだもん: <最後のオチをつける。>
                                             """},
@@ -70,7 +70,7 @@ def summarize_text(subreddit, text):
 
     return response.choices[0].message.content
 
-def get_hot_posts_with_comments(subreddit_name, limit=10):
+def get_hot_posts_with_comments(subreddit_name, limit=3, time_filter="week"):
     reddit_client_id = os.getenv("REDDIT_CLIENT_ID")
     reddit_client_secret = os.getenv("REDDIT_CLIENT_SECRET")
     reddit_user_agent = os.getenv("REDDIT_USER_AGENT")
@@ -81,15 +81,23 @@ def get_hot_posts_with_comments(subreddit_name, limit=10):
         user_agent=reddit_user_agent,
     )
     subreddit = reddit.subreddit(subreddit_name)
-    # best_posts = list(subreddit.top(limit=limit, time_filter="week"))
-    hot_posts = list(subreddit.hot(limit=limit, time_filter="week"))
+    best_posts = list(subreddit.top(limit=limit, time_filter=time_filter))
+    # hot_posts = list(subreddit.hot(limit=limit))
 
-    all_posts = hot_posts # + best_posts
+    all_posts = best_posts
 
     all_posts_text = ""
     for post in all_posts:
+        post_date = datetime.utcfromtimestamp(post.created_utc)
+
         # 各投稿の情報を結合
-        post_text = f"タイトル: {post.title}\nURL: {post.url}\nスコア: {post.score}\nコメント数: {post.num_comments}\n本文: {post.selftext}\n"
+        post_text = f"タイトル: {post.title}\n"
+        post_text += f"URL: {post.url}\n"
+        post_text += f"投稿日時: {post_date:%Y/%m/%d %H:%M:%S}\n"
+        post_text += f"スコア: {post.score}\n"
+        post_text += f"コメント数: {post.num_comments}\n"
+        post_text += f"本文:\n{post.selftext}\n"
+        print(post_text)
 
         # コメントリストを取得して結合
         comments = post.comments
